175a0950d9b769582380bf635e931e7c9a512dad
Motonei <letinamotonei@gmail.com>
2026-04-15 16:21:28 +0300

    update

---
+++ lists.py
@@ -0,0 +1,12 @@
+# create a list of fruits
+fruits=['mango','Banana','Orange','lemon']
+print(type(fruits))
+print(fruits)
+# update item
+fruits[1]='Apple'
+print(fruits)
+print(fruits[2])
+# slice
+print(fruits[2:])
+
+
---
+++ task2.py
@@ -0,0 +1,26 @@
+# Convert a float to an integer with an inbuilt function in Python
+# temp = 56.8926 to 57
+temp=56.8926
+temp=round(temp)
+print(temp)
+# Convert the float below to give the results as follows
+# temp = 56.8926 to 56.89 
+temp=56.8926
+temp=round(temp,2)
+print(temp)
+# Convert the float below to give the results as follows
+# temp = 56.8926 to 56.893 
+temp=56.8926
+temp=round(temp,3)
+print(temp)
+# Convert the float below to give the results as follows
+# temp=56.8926 to 8.926 
+temp=str(temp)
+temp=temp.split('.')[1]
+print(temp)
+temp=('8'+'.'+'926')
+print(temp)
+temp=float(temp)
+print(temp)
+
+# NB: Use string  slice & concatenation, but have result as float 
